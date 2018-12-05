3104e15d229b626b51d36bc4dc8a876b0d597308
Vadim-Karpenko3 <vadim@minimuminc.com>
2018-12-05 04:57:17 +0200

    Fixed urls.py error

--- admin-multilanguage/urls.py
+++ admin-multilanguage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from languages import views
+from admin-multilanguage import views
 
-app_name = "languages"
+app_name = "admin-multilanguage"
 urlpatterns = [
     path('change_language/', views.ChangeLanguageView.as_view(), name='change_language'),
 ]
