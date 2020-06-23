fd438f292bd3fc28e4ff944a0042a999942d3dc8
Vadim-Karpenko3 <vadim@minimuminc.com>
2020-06-23 20:49:45 +0300

    Add docstring, fix HttpResponse error and add minor improvements

--- admin_multilanguage/views.py
+++ admin_multilanguage/views.py
@@ -1,15 +1,34 @@
 from django.views.generic import View
 from django.shortcuts import redirect
 from django.conf import settings
+from django.urls import reverse
+from django.core.exceptions import ValidationError
 
 class ChangeLanguageView(View):
+    """
+    View to change language of the admin page by changing it's value that is stored in sessions.
+
+    **GET PARAMS**
+
+    ``language``
+        str: A unique code of the language
+
+    ``current_path``
+        str: The current path of the user when request was made. It used to redirect user back to the same place.
+    """
+
     def get(self, request):
-        language = request.GET.get('language', None)
+        # Get request parameters
+        language = request.GET.get('language', settings.LANGUAGE_CODE)
         current_path = request.GET.get('current_path', None)
+        # Check if settings.LANGUAGES is valid
+        if not settings.LANGUAGES:
+            raise ValidationError("\"settings.LANGUAGES\" is empty or impoperly configured")
+        # Check if new language code is available in settings.LANGUAGES
         for setting_language in settings.LANGUAGES:
-            if language == setting_language[0]:
+            if language.lower() == setting_language[0].lower():
+                request.session['_language'] = language
                 if current_path:
-                    request.session['_language'] = language
                     return redirect(current_path)
-                else:
-                    raise ValidationError("\"current_path\" is impoperly configured")
+        # If no current_path specified, user will be redirected to the admin main page
+        return redirect("admin:index")
